fee851e73646bf0df2b42e47d88bc400c9e1e85c
RDNyel <al12542062@unadmexico.mx>
2019-11-18 04:16:07 -0600

    +1:

--- js/app.js
+++ js/app.js
@@ -18,3 +18,15 @@ $offCanvas.find('li').click(function (ev) {
 
 
 
+// more click
+$('.card-profile-stats-more-link').click(function(e){
+	e.preventDefault();
+	if ( $(".card-profile-stats-more-content").is(':hidden') ) {
+	  $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+	} else {
+	  $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+	}
+	$(this).next('.card-profile-stats-more-content').slideToggle();
+  });
+  
+  
